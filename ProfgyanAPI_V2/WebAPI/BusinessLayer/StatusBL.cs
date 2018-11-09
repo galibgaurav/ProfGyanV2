48406354359ea5f6f75803c96aedf41630c3554f
Galib Gaurav <galibgaurav@gmail.com>
2018-11-09 20:36:46 +0530

    interface and BL

---
+++ ProfgyanAPI_V2/WebAPI/BusinessLayer/StatusBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using BusinessDataModel;
+using IBusinessLayer;
+
+namespace BusinessLayer
+{
+    public class StatusBL : IStatusBL
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        public Status GetStatus(int id)
+        {
+            return unitOfWork.StatusRepository.GetByID(id);
+        }
+
+        public Status AddStatus(Status status)
+        {
+            var result = unitOfWork.StatusRepository.Insert(status);
+            unitOfWork.Save();
+            return result;
+        }
+
+        public void UpdateStatus(Status status)
+        {
+            unitOfWork.StatusRepository.Update(status);
+            unitOfWork.Save();
+        }
+
+        public void DeleteStatus(int id)
+        {
+            unitOfWork.StatusRepository.Delete(id);
+            unitOfWork.Save();
+        }
+
+        public IEnumerable<Status> GetSocialMedia(Expression<Func<Status, bool>> filter = null,
+            Func<IQueryable<Status>, IOrderedQueryable<string>> orderBy = null, string includeProperties = "")
+        {
+            var result = unitOfWork.StatusRepository.Get(null, null, "");
+            return result;
+        }
+    }
+}
+
+
---
+++ ProfgyanAPI_V2/WebAPI/IBusinessLayer/IStatusBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BusinessDataModel;
+
+namespace BusinessLayer
+{
+    public interface IStatusBL
+    {
+        Status AddStatus(Status status);
+        void DeleteStatus(int id);
+        IEnumerable<Status> GetSocialMedia(Expression<Func<Status, bool>> filter = null, Func<IQueryable<Status>, IOrderedQueryable<string>> orderBy = null, string includeProperties = "");
+        Status GetStatus(int id);
+        void UpdateStatus(Status status);
+    }
+}
