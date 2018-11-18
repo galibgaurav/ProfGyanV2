12e1a2015da4ca6d3f41d4fcab5a29fe92c62123
Galib Gaurav <galibgaurav@gmail.com>
2018-11-18 23:31:39 +0530

    Added appointment controller

--- ProfgyanAPI_V2/WebAPI/WebAPI/Controllers/AppointmentController.cs
+++ ProfgyanAPI_V2/WebAPI/WebAPI/Controllers/AppointmentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using DataModelDTO;
+using IBusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,31 +12,49 @@ namespace WebAPI.Controllers
 {
     public class AppointmentController : ApiController
     {
-        // GET: api/Appointment
-        public IEnumerable<string> Get()
+        public IAppointmentBL appointmentBL;
+        public AppointmentController(IAppointmentBL _appointmentBL)
         {
-            return new string[] { "value1", "value2" };
+            appointmentBL = _appointmentBL;
+        }
+
+        // GET: api/Appointment
+        public IEnumerable<DataModelDTO.Appointment> Get()
+        {
+
+            var appointmentList = appointmentBL.GetAppointment(null, null, String.Empty);
+            var result = Mapper.Map<IEnumerable<DataModelDTO.Appointment>>(appointmentBL);
+            return result;
         }
 
         // GET: api/Appointment/5
-        public string Get(int id)
+        public Appointment Get(string id)
         {
-            return "value";
+            var appointment = appointmentBL.GetAppointment(id);
+            var result = Mapper.Map<DataModelDTO.Appointment>(appointment);
+            return result;
         }
 
         // POST: api/Appointment
-        public void Post([FromBody]string value)
+        public Appointment Post(DataModelDTO.Appointment appointment)
         {
+            var postData = Mapper.Map<BusinessDataModel.Appointment>(appointment);
+            var result = appointmentBL.AddAppointment(postData);
+            var resultToReturn = Mapper.Map<DataModelDTO.Appointment>(result);
+            return resultToReturn;
         }
 
-        // PUT: api/Appointment/5
-        public void Put(int id, [FromBody]string value)
+        // PUT: api/Appointment
+        public void Put(DataModelDTO.Appointment appointment)
         {
+            var putData = Mapper.Map<BusinessDataModel.Appointment>(appointment);
+            appointmentBL.UpdateAppointment(putData);
         }
 
         // DELETE: api/Appointment/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            appointmentBL.DeleteAppointment(id);
         }
     }
 }
